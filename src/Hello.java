15799e77d42cf70e748133b06dd0e21f6f9b73c1
유경원 <@DESKTOP-QU5R6U9>
2020-03-17 21:32:49 +0900

    second

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
         System.out.println("Hello, world.");
-        System.out.println("Hello, world2.");
+        System.out.println("Hello, world222.");
 	}
 
 }   
